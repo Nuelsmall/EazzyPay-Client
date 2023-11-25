628d0250b79844f997ede499e573b6a82b8300d9
Nuelsmall <nuelsmall30@gmail.com>
2023-11-25 20:11:01 +0100

    Home page completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Header } from "./Header/Header";
 import { Hero } from "./Hero/Hero";
-
+import { Footer } from "./Footer/footer";
 
 function App() {
   return (
     <div>
      <Header />
      <Hero />
+     <Footer />
     </div>
   );
 }
